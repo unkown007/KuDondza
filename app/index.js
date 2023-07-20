ad9ecb90fea66ea474f459d42833c6e14f80dadc
Ludmila Emilia <95902126+ludyy908@users.noreply.github.com>
2023-07-20 09:14:13 +0200

    update

--- app/index.js
+++ app/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { View, Image, TouchableOpacity } from "react-native";
+import {COLORS} from '../constants/theme'
 import ProfilePic from "../components/common/profilepic/ProfilePic";
 
 const Home = () => {
-    const router = useRouter();
-
     return (
         <View style={{ flex: 1, backgroundColor: COLORS.white }}>
             <ProfilePic width = {50} height = {50}/>
--- components/common/profilepic/ProfilePic.jsx
+++ components/common/profilepic/ProfilePic.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { View, Image, TouchableOpacity } from "react-native";
-import imageContainer from './ProfilePic.style'
+import imageContainer from './ProfilePic.style';
+import R from "../../../assets/icons/R.png"
 
 const ProfilePic = ({width, height}) => {
     return(
         <View>
             <TouchableOpacity>
                 <Image 
-                    source={require('assets/icons/icons8-close-24.png')}
+                    source={R}
                     style = {imageContainer.dimensions(width, height)}
                 />
             </TouchableOpacity>
