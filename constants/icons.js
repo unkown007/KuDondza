29b0714528061e9693a62e7f680176f5bf046692
Amarilda Chihepe <amariyldachiheppe@gmail.com>
2023-07-21 04:14:09 +0200

    icons

--- constants/icons.js
+++ constants/icons.js
@@ -13,14 +13,9 @@ import plus from "../assets/icons/plus.png";
 import minus from "../assets/icons/minus.png";
 import Options from '../assets/icons/icons8-three-dots-24.png'
 import Back from '../assets/icons/icons8-back-32.png'
-import icon from '../assets/icons/icons8-plus-and-minus-50.png'
-import icon2 from '../assets/icons/bookmark.png'
-import icon3 from '../assets/icons/share.png'
-import PostDbt from "../assets/icons/icons8-plus-50.png"
+
 
 export {
-    PostDbt,
-    icon,icon2,icon3,
     plus,
     minus,
     backBlue,
